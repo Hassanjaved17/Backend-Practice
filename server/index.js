12b5aa9de65260e52d306f226a08dec965235479
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-09 00:20:57 +0500

    Implement basic Express server with a root route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-console.log("server is running");
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
